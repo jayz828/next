b04e233d3a3f7dd8878d264a4224b328bb42748f
Jason Natividad <jainatividad@yahoo.com>
2017-05-07 17:21:12 -0700

    Add express and signup page now working

---
+++ app.js
@@ -0,0 +1,80 @@
+/**
+ * Created by jasonnatividad on 5/7/17.
+ */
+
+var express = require('express');
+
+
+var path = require('path');
+
+
+var app = express();
+
+
+var port = 3000;
+
+// app.use setups middleware, used by express first.
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+
+
+
+// request - information coming from the browser
+// response - is what we deal with
+
+
+
+// ROUTES
+
+
+app.get('/', function(req,res) {
+
+    res.sendFile(path.join(__dirname, 'src/index.html'))
+
+});
+
+
+app.get('/Books', function(req,res) {
+
+    res.send('Hello Books');
+
+});
+
+
+app.listen(port, function(err) {
+
+    console.log('Running server on port ' + port);
+
+});
+
+//var path = require('path');
+//
+//var open = require('open');
+
+
+
+
+
+
+
+
+//app.get('/', function(req, res){
+//
+//    res.sendFile(path.join(__dirname, '../src/index.html'))
+//});
+
+//
+//app.listen(port, function(err){
+//
+//    if (err) {
+//        console.log(err);
+//    } else {
+//        open('http://localhost:' + port);
+//    }
+//
+//
+//});
+
+
